2261ea8e897af3691cd7858bacf7b065b2f0ca24
centreatlas <centreatlas@gmail.com>
2022-05-09 15:36:37 +0100

    init commit

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  get 'pages/home'
+  get 'pages/about', as: :about_pages
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  root "pages#home"
+end
